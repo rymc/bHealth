9a588401df1179ae45bae604d19d2b7edcb5f9d7
Ryan McConville <ryan.mcconville@bristol.ac.uk>
2019-04-16 12:42:50 +0100

    Initial commit

---
+++ src/example1.py
@@ -0,0 +1,57 @@
+from transforms import Transforms
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../data/realistic_sensor_displacement/subject2_ideal.log', sep='\t', header=None)
+
+acc= df.ix[:, [0, 68,69,70]]
+acc.columns=['ts', 'x','y','z']
+acc['ts'] = pd.to_datetime(acc['ts'], unit='s')
+
+ts = acc[['ts']].values
+xyz = acc[['x','y','z']].values
+labels = df[df.columns[-1]].values
+
+x = []
+y = []
+
+winlength_seconds = 3
+overlap_seconds = 1
+print("Window size of "+str(winlength_seconds)+" seconds and overlap of "+str(float(overlap_seconds) / winlength_seconds)+"%")
+samples_per_sec = 50
+winlength = samples_per_sec * winlength_seconds
+current = winlength
+overlap = samples_per_sec * overlap_seconds
+
+transform = Transforms(window_length=winlength, window_overlap=overlap)
+print("Use number of zero crossings, spectral entropy as features..")
+feature_transforms = [transform.zero_crossings, transform.spec_entropy]
+
+while True:
+    windowed_raw = transform.slide(xyz)
+    if len(windowed_raw) > 0:
+        try:
+            windowed_features = [ts[transform.current_position][0]]
+        except Exception as e:
+            break
+        for function in feature_transforms:
+            windowed_features.extend((np.apply_along_axis(function, 0, windowed_raw).tolist()))
+        x.append(windowed_features)
+
+        windowed_raw_labels = transform.slide(labels, update=False)
+        most_freq_label = np.bincount(windowed_raw_labels).argmax()
+        y.append(most_freq_label)
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, train_test_split
+
+clf = RandomForestClassifier(n_estimators=250,)
+cv_score = cross_val_score(clf, x, y, cv=5)
+print("5 CV accuracy "+str(np.mean(cv_score)))
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.6, random_state=0)
+clf = RandomForestClassifier(n_estimators=250,)
+clf.fit(X_train, y_train)
+tt_score = clf.score(X_test, y_test)
+print("Train / test split accuracy "+str(tt_score))
---
+++ src/transforms.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+class Transforms:
+
+
+    def __init__(self, window_length, window_overlap):
+        self.window_length = window_length
+        self.current_position = 0
+        self.window_overlap = window_overlap
+
+    @staticmethod
+    def zero_crossings(x):
+        mean = np.mean(x)
+        sign = [1,0]
+        direction = 0
+        count_zc = 0;
+        if x[0] >= mean:
+            direction = 1
+
+        for i in range(len(x)):
+            if (x[i] >= mean and direction == 0) or (x[i] < mean and direction == 1):
+                direction = sign[direction]
+                count_zc += 1
+        return count_zc
+
+    @staticmethod
+    def interq(x):
+        p25 = np.percentile(x, 25)
+        p75  = np.percentile(x, 75)
+        interquartile = p25 - p75
+        return interquartile
+
+    @staticmethod
+    def skewn(x):
+        skewness = scipy.stats.mstats.skew(x)              
+        skewness = skewness.data.flatten()[0]
+        return skewness
+
+    @staticmethod
+    def spec_energy(x):
+        f = np.fft.fft(x)  
+        F = abs(f)
+        return sum(np.square(F))  
+
+    @staticmethod
+    def spec_entropy(x):
+        f = np.fft.fft(x)  
+        F = abs(f)
+        sumf = sum(F)
+        if sumf == 0:
+            sumf = 1
+        nf = F/sumf
+        min_nf = 1
+        if (min(nf) != max(nf)) and (min(nf) != 0):  
+            min_nf = min(m for m in nf if m > 0)
+
+        logf = np.log((nf+min_nf))
+        spectral_entropy = -1*sum(nf*logf)          
+        return spectral_entropy
+
+    @staticmethod
+    def p25(x):
+        return np.percentile(x, 25)
+
+    @staticmethod
+    def p75(x):
+        return np.percentile(x, 75)
+
+    @staticmethod
+    def kurtosis(x):
+        return scipy.stats.mstats.kurtosis(x)
+
+    def slide(self, x, update=True):
+        window = x[self.current_position-self.window_length:self.current_position]
+        if len(window) > 0:
+            if len(window.shape) > 1:
+                window = window[~np.isnan(window).any(axis=1)]
+            else:
+                window = window[~np.isnan(window)]
+        if update:
+            self.current_position += self.window_overlap
+        return window
