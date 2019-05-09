6ef2bec375fc30b2cfd25bd066b3576615997258
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-05-09 12:03:57 +0100

    Reorganised example 2 - Same modifications than in example 1 - Missing the separation between get raw data and preprocess data at this point

--- src/example2.py
+++ src/example2.py
@@ -1,105 +1,118 @@
-from transforms import Transforms
-import pandas as pd
-import numpy as np
-import os
+def get_raw_ts_X_y():
+    import os
+    import numpy as np
+    import pandas as pd
+    from datawrapper.house import HouseVisit
+    from datawrapper.utils import get_all_paths, df_start_end_to_time
+    datasets_path = '../data/houses'
+    path_expression = r".*{0}(?P<hid>\w+){0}(?P<vid>\w+){0}sphere".format(
+        os.sep)
+    all_bson_paths = get_all_paths(datasets_path,
+                                   path_expression=path_expression)
 
-from datawrapper.house import HouseVisit
-from datawrapper.utils import get_all_paths, df_start_end_to_time
+    visit = HouseVisit('9665', all_bson_paths[0])
 
-datasets_path = '../data/houses'
-all_bson_paths = get_all_paths(datasets_path, path_expression=r".*{0}(?P<hid>\w+){0}(?P<vid>\w+){0}sphere".format(os.sep))
+    df_ann = visit.get_annotations()
 
-visit = HouseVisit('9665', all_bson_paths[0])
+    time_intervals = df_ann[df_ann['tier'] == 'Experiment'][['start', 'end']]
+    rssi_list = visit.get_rssi(time_intervals=time_intervals)
 
-df_items = visit.get_hypercat(os.path.join(visit.root, 'hypercat-db'))
-df_ann = visit.get_annotations()
+    df_rssi = pd.concat(rssi_list)
+    df_rssi = df_rssi.resample('1S').agg(np.nanmean)
+    df_loc = df_ann[df_ann['tier'] == 'Location'][['start', 'end', 'label']]
+    df_loc = df_start_end_to_time(df_rssi.index, df_loc)
+    df_loc['label'] = df_loc['label'].cat.remove_unused_categories()
 
-time_intervals = df_ann[df_ann['tier'] == 'Experiment'][['start', 'end']]
-rssi_list = visit.get_rssi(time_intervals=time_intervals)
+    loc_mask = ~df_loc['label'].isna()
+    df_rssi = df_rssi[loc_mask]
+    df_loc = df_loc[loc_mask]
 
-df_rssi = pd.concat(rssi_list)
-try:
-    href_loc_map = visit.get_href_locations()
-except AttributeError as e:
-    print(e)
-df_rssi = df_rssi.resample('1S').agg(np.nanmean)
-df_loc = df_ann[df_ann['tier'] == 'Location'][['start', 'end', 'label']]
-df_loc = df_start_end_to_time(df_rssi.index, df_loc)
-df_loc['label'] = df_loc['label'].cat.remove_unused_categories()
+    visit.y = df_loc['label'].cat.codes.values
+    visit.X = df_rssi.values
 
-loc_mask = ~df_loc['label'].isna()
-df_rssi = df_rssi[loc_mask]
-df_loc = df_loc[loc_mask]
+    visit.features = df_rssi.columns
+    visit.labels = df_loc['label'].cat.categories
 
-visit.y = df_loc['label'].cat.codes.values
-visit.X = df_rssi.values
+    X = visit.X
+    y = visit.y
+    return (), X, y
 
-labels = df_loc['label'].cat.categories
-visit.features = df_rssi.columns
-visit.labels = df_loc['label'].cat.categories
 
-x = visit.X
-y = visit.y
+def preprocess_X_y(ts, X, y):
+    return X, y
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, cross_val_predict
-from sklearn.model_selection import StratifiedKFold, cross_val_score, \
-        GridSearchCV, train_test_split
 
-# Create train and test partitions
-skf = StratifiedKFold(n_splits=2, shuffle=False)
-train_index, test_index = skf.split(x, y).__next__()
-print(train_index)
-print(test_index)
-X_train, X_test = x[train_index], x[test_index]
-y_train, y_test = y[train_index], y[test_index]
+def split_train_test(X, y):
+    from sklearn.model_selection import StratifiedKFold
+    # Create train and test partitions
+    skf = StratifiedKFold(n_splits=2, shuffle=False)
+    train_index, test_index = skf.split(X, y).__next__()
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    return (X_train, y_train), (X_test, y_test)
 
-# Create cross-validation partitions from training
-# This should select the best set of parameters
-cv = StratifiedKFold(n_splits=5, shuffle=False)
 
-models = {'rf': {'model': RandomForestClassifier(),
-                 'parameters': {'n_estimators': [200, 250, 300],
-                                'min_samples_leaf': [1, 5, 10, 20]}},
-          'lr': {'model': LogisticRegression(penalty='l2'),
-                 'parameters': {'C': [0.01, 0.1, 1, 10, 100]}},
-          'svc': {'model': SVC(probability=True),
-                  'parameters': [{'kernel': ['rbf'],
-                                  'gamma': [1e-3, 1e-4],
-                                  'C': [1, 10, 100, 1000]},
-                                 {'kernel': ['linear'],
-                                  'C': [1, 10, 100, 1000]}]},
-          'svc-rbf': {'model': SVC(probability=True),
+def get_classifier_grid():
+    import numpy as np
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.svm import SVC
+    from sklearn.model_selection import StratifiedKFold, GridSearchCV
+    # Create cross-validation partitions from training
+    # This should select the best set of parameters
+    cv = StratifiedKFold(n_splits=5, shuffle=False)
+
+    models = {'rf': {'model': RandomForestClassifier(),
+                     'parameters': {'n_estimators': [200, 250],
+                                    'min_samples_leaf': [1, 5, 10]}},
+              'lr': {'model': LogisticRegression(penalty='l2'),
+                     'parameters': {'C': [0.01, 0.1, 1, 10, 100]}},
+              'svc': {'model': SVC(probability=True),
                       'parameters': [{'kernel': ['rbf'],
                                       'gamma': [1e-3, 1e-4],
-                                      'C': [1, 10, 100, 1000]}, ]},
-          }
+                                      'C': [1, 10, 100, 1000]},
+                                     {'kernel': ['linear'],
+                                      'C': [1, 10, 100, 1000]}]},
+              'svc-rbf': {'model': SVC(probability=True),
+                          'parameters': [{'kernel': ['rbf'],
+                                          'gamma': [1e-3, 1e-4],
+                                          'C': [1, 10, 100, 1000]}, ]},
+              }
+
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.impute import SimpleImputer
+    from sklearn.pipeline import Pipeline
+    classifier_name = 'rf'
+
+    steps = [('imputer', SimpleImputer(missing_values=np.nan,
+                                       strategy='mean')),
+             ('scaler', StandardScaler()),
+             ('clf', models[classifier_name]['model'])]
+
+    pipeline = Pipeline(steps)
+
+    pipeline_parameters = {'clf__' + key: value for key, value in
+                           models[classifier_name]['parameters'].items()}
+
+    clf_grid = GridSearchCV(pipeline, param_grid=pipeline_parameters, cv=cv,
+                            refit=True)
+    return clf_grid
 
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-classifier_name = 'rf'
+def print_summary(clf_grid, X_test, y_test):
+    import numpy as np
+    print('Best parameters are: {}'.format(clf_grid.best_params_))
+    print("CV accuracy "+str(np.mean(clf_grid.cv_results_['mean_test_score'])))
 
-steps = [('imputer', SimpleImputer(missing_values=np.nan,
-                                   strategy='mean')),
-         ('scaler', StandardScaler()),
-         ('clf', models[classifier_name]['model'])]
+    # The best model was fitted on the full training data, here tested only
+    tt_score = clf_grid.score(X_test, y_test)
+    print("Train / test split accuracy "+str(tt_score))
 
-pipeline = Pipeline(steps)
 
-pipeline_parameters = {'clf__' + key: value for key, value in
-                       models[classifier_name]['parameters'].items()}
-
-grid = GridSearchCV(pipeline, param_grid=pipeline_parameters, cv=cv,
-                    refit=True)
-
-grid.fit(X_train, y_train)
-print('Best parameters are: {}'.format(grid.best_params_))
-print("5 CV accuracy "+str(np.mean(grid.cv_results_['mean_test_score'])))
-
-# The best model was fitted on the full training data, here it is tested only
-tt_score = grid.score(X_test, y_test)
-print("Train / test split accuracy "+str(tt_score))
+if __name__ == '__main__':
+    ts, X, y = get_raw_ts_X_y()
+    X, y = preprocess_X_y(ts, X, y)
+    (X_train, y_train), (X_test, y_test) = split_train_test(X, y)
+    clf_grid = get_classifier_grid()
+    clf_grid.fit(X_train, y_train)
+    print_summary(clf_grid, X_test, y_test)
