1884b182715e4e823329bafe2bf243333357c577
Michal Kozlowski <m.kozlowski@bristol.ac.uk>
2019-06-10 12:25:40 +0100

    Test

--- src/metrics.py
+++ src/metrics.py
@@ -75,23 +75,23 @@ class Metrics:
 
         return label_change_array
 
-    @staticmethod
-    def average_time_between_labels(labels, timestamps):
-        # assuming a finite set of ordinal labels
-        unique_lab, counts_lab = np.unique(labels, return_counts=True)
-
-        number_of_instances = len(labels)
-        number_of_labels = len(unique_lab)
-        total_time_in_window = timestamps[-1] - timestamps[0]
-
-        label_change_array = np.zeros((number_of_labels, number_of_labels))
-
-        for idx_outer in range(number_of_labels):
-            lab_instance_outer = labels[idx_outer]
-            if labels[idx] == lab_instance_outer and labels[idx + 1] == lab_instance_inner:
-                label_change_array[int(idx_outer), int(idx_inner)] += 1
-
-        return label_change_array
+    # @staticmethod
+    # def average_time_between_labels(labels, timestamps):
+    #     # assuming a finite set of ordinal labels
+    #     unique_lab, counts_lab = np.unique(labels, return_counts=True)
+    #
+    #     number_of_instances = len(labels)
+    #     number_of_labels = len(unique_lab)
+    #     total_time_in_window = timestamps[-1] - timestamps[0]
+    #
+    #     label_change_array = np.zeros((number_of_labels, number_of_labels))
+    #
+    #     for idx_outer in range(number_of_labels):
+    #         lab_instance_outer = labels[idx_outer]
+    #         if labels[idx] == lab_instance_outer and labels[idx + 1] == lab_instance_inner:
+    #             label_change_array[int(idx_outer), int(idx_inner)] += 1
+    #
+    #     return label_change_array
 
     def establish_sampling_frequency(self, timestamps):
         sampling_frequency = []
