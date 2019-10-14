3b7e06a769069fcabc5e7c7920daa19fe560a118
Miquel Perello Nieto <perello.nieto@gmail.com>
2019-10-14 14:35:11 +0100

    Updated download data script

--- scripts/download-rssi-acc.sh
+++ scripts/download-rssi-acc.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 wget -O rssi-acc-data.zip https://ndownloader.figshare.com/files/14220518
 unzip rssi-acc-data.zip
-mkdir data
-mkdir data/acc_loc_data
-mv ble-accelerometer-indoor-localisation-measurements data/acc_loc_data
-mv data/ ../
+mkdir -p ../data/acc_loc_data/
+mv ble-accelerometer-indoor-localisation-measurements ../data/acc_loc_data/
